25b5f305fc670d551093fbb96017d98ba06cb33a
Iago Gomes <iagogb@alu.ufc.br>
2019-05-31 22:46:33 -0300

    Commit Inicial

---
+++ src/main/java/br/com/casamovel/CasaMovelApplication.java
@@ -0,0 +1,13 @@
+package br.com.casamovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaMovelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaMovelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/casamovel/CasamovelApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.casamovel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CasamovelApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
